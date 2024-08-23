7678060e365083bb1cca5f8ab0a77c6b327783d7
David Anderson <david.softwares1@gmail.com>
2024-08-22 23:09:26 -0300

    Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        // Estudar a classe String que representa texto na aplicação
+
+
+
+        // Declarando as variáveis como exemplo
+        int idade = 43;
+        String nomePrimeiro = "David";
+        String nomeSegundo = "Anderson";
+        int anoNascimento = 1981;
+
+        // Imprimindo os resultados
+        System.out.println(idade);
+        System.out.println(nomePrimeiro);
+        System.out.println(nomeSegundo);
+        System.out.println(anoNascimento);
     }
 }
